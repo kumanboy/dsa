502f463e0a9760c762c071d9c238d3fc8140d99a
kumanboy <husandavronov13@gmail.com>
2024-11-28 11:57:09 +0500

    O(1) end

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-// const studentDb = ['husan', 'aslbek','kamila','lola','bobur']
-//
-// function findStudents(allStudents, studentName) {
-//     for (let i = 0; i < allStudents.length; i++) {
-//         if (allStudents[i] === studentName) {
-//             console.log(`Found ${studentName}`)
-//         }
-//     }
-// }
-//
-// findStudents(studentDb, 'kamol')
-
-
-// BIG O = O(n)
+// BIG O = O(1)
 
 const products = ['tuxum','non','qulupnay','sut','banan']
 
-const kerakliMaxsulotIzlash = (maxsulot) =>{
-    for (let i = 0; i < products.length; i++) {
-        if (products[i] === maxsulot) {
-            console.log(`Topilgan maxsulot: ${maxsulot}`)
-        }
-    }
+const aynanBittaMaxsulotIzlash = (maxsulotar,index) =>{
+    return maxsulot[index]
 }
 
-kerakliMaxsulotIzlash('sut')
+console.log(aynanBittaMaxsulotIzlash(products,2))
 
-
-//0010101
-//0123456789ABCDEF
